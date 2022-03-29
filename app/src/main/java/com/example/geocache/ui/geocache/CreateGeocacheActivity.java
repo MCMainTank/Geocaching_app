3bae63832f74b79af1dc48ca29a41a6252eba5d5
MCMainTank <qiyelin1999@163.com>
2022-03-29 13:48:57 +0800

    v1.0.6

--- app/src/main/java/com/example/geocache/ui/geocache/CreateGeocacheActivity.java
+++ app/src/main/java/com/example/geocache/ui/geocache/CreateGeocacheActivity.java
@@ -1,18 +1,41 @@
 package com.example.geocache.ui.geocache;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.geocache.R;
 
+import okhttp3.OkHttpClient;
+
 public class CreateGeocacheActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private OkHttpClient okHttpClient;
+    private String responseString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_geocache);
+        Button button_1 = findViewById(R.id.button3);
+        editText = findViewById(R.id.location_description);
+        button_1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String geocacheDescription = editText.getText().toString();
 
+                new Thread() {
+                    @Override
+                    public void run() {
+
+                    }
+                }.start();
+            }
+        });
     }
 
 }
